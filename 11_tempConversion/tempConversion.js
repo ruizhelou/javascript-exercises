161aafc22a41c50cb11be89f6ea69da66e960e83
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2025-09-26 20:12:32 +1200

    Complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9
+  return Number.isInteger(celsius) ? celsius : parseFloat(celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * 9/5 + 32
+  return Number.isInteger(fahrenheit) ? fahrenheit : parseFloat(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
