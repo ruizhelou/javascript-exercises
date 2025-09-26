724477557c3828519ce23054db440b282efae46a
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2025-09-26 19:50:25 +1200

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0) return 'ERROR'
+    if(typeof(a) !== "number" || typeof(b) !== "number") return 'ERROR'
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR'
+    let startIndex
+    let endIndex
+    if(a > b) {
+        startIndex = b
+        endIndex = a
+    }
+    else {
+        startIndex = a
+        endIndex = b
+    }
+    let total = 0
+    for(let i = startIndex; i <= endIndex; i++) {
+        total += i
+    }
+    return total
 };
 
 // Do not edit below this line
