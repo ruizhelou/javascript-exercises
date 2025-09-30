a2de8e0f2b5c74995d22c0ce419cea668e829155
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2025-09-30 20:39:08 +1300

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanString = str
+        .replaceAll("!", "")
+        .replaceAll(",", "")
+        .replaceAll(".", "")
+        .split(" ")
+        .join("")
+        .toLowerCase()
+    const cleanStringReversed = cleanString.split("").reverse().join("")
+    return cleanString === cleanStringReversed
 };
 
 // Do not edit below this line
