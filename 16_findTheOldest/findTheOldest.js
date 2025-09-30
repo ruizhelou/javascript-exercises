80eea32fd8c5a140e35507465f1cfba69e7b3b4d
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2025-09-30 21:10:06 +1300

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeople) {
+    return arrayOfPeople.reduce((oldestSoFar, nextPerson) => {
+        if(!("yearOfDeath" in oldestSoFar)) {
+            oldestSoFar.yearOfDeath = new Date().getFullYear()
+        }
+        const oldestSoFarAge = oldestSoFar.yearOfDeath - oldestSoFar.yearOfBirth
+        const nextPersonAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth
+        return oldestSoFarAge > nextPersonAge ? oldestSoFar : nextPerson
+    })
 };
+
+findTheOldest([
+      {
+        name: "Carly",
+        yearOfBirth: 1066,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ])
 
 // Do not edit below this line
 module.exports = findTheOldest;
