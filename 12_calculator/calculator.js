d3e791acec5e458ee755a9f4bfa0203b512379ea
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2025-09-30 20:28:08 +1300

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sumSoFar, nextNumber) => sumSoFar + nextNumber, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((productSoFar, nextNumber) => productSoFar * nextNumber, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let factorial = 1
+  while(n > 1) {
+    factorial *= n
+    n--
+  }
+  return factorial
 };
 
 // Do not edit below this line
