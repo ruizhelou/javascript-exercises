b8749304705a3c5fa32de8cc45d72610d098e965
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2026-01-12 23:55:15 +1300

    Complete totalIntegers.js

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,41 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(arrayOrObject) {
+    // if(Number.isInteger(arrayOrObject)) {
+    //     return 1
+    // }
+    // if(typeof(arrayOrObject) !== 'object') {
+    //     return 0
+    // }
+
+    // let total = 0
+    // if(Array.isArray(arrayOrObject)) {
+    //     for(const subArrayOrObject of arrayOrObject) {
+    //         total += totalIntegers(subArrayOrObject)
+    //     }
+    // } else {
+    //     for(const subArrayOrObject of Object.values(arrayOrObject)) {
+    //         total += totalIntegers(subArrayOrObject)
+    //     }
+    // }
+    // return total
+    if(typeof(arrayOrObject) !== 'object') {
+        return undefined
+    }
+    let array = null
+    if(Array.isArray(arrayOrObject)) {
+        array = arrayOrObject
+    } else { // Is object
+        array = Object.values(arrayOrObject)
+    }
+
+    let total = 0;
+    for(const thing of array) {
+        if(Number.isInteger(thing)) {
+            total += 1
+        } else if(typeof(thing) === 'object' && thing !== null) {
+            total += totalIntegers(thing)
+        }
+    }
+    return total;
 };
   
 // Do not edit below this line
