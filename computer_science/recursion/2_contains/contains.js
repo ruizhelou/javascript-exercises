5c4c8eeb86ff49e7eedfd4266a211aa2de08d9e9
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2026-01-12 23:17:43 +1300

    Complete contains.js

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,22 @@
-const contains = function() {
-  
+const contains = function(object, targetValue) {
+    if(object === targetValue) {
+        return true
+    } else if(Number.isNaN(object) && Number.isNaN(targetValue)) {
+        return true
+    } 
+    // Leaf node is non-object
+    else if(typeof(object) !== 'object') {
+        return false
+    } else if(object === null || object === undefined) {
+        return false
+    }
+    for(const subObject of Object.values(object)) {
+        if (contains(subObject, targetValue)) {
+            return true
+        }
+    }
+    return false
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
