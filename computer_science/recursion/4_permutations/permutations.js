d5c7831f4cda58105f246b8dd1f7ee0c5f63cc4e
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2026-01-13 09:02:36 +1300

    Complete permutations.spec.js

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,24 @@
-const permutations = function() {
-  
+const permutations = function(array, permutationsArray = [], permutationSoFar = []) {
+    if(array.length === 0) {
+        permutationsArray.push(permutationSoFar)
+        return permutationsArray
+    }
+    if(array.length === 1) {
+        let copy = [...permutationSoFar]
+        copy.push(array[0])
+        permutationsArray.push(copy)
+        return permutationsArray
+    }
+
+    for(let i = 0; i < array.length; i++) {
+        let permutationSoFarCopy = [...permutationSoFar]
+        permutationSoFarCopy.push(array[i])
+        let arrayCopy = [...array]
+        arrayCopy.splice(i, 1)
+        permutations(arrayCopy, permutationsArray, permutationSoFarCopy)
+    }
+    return permutationsArray
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
