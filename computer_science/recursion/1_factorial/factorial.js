8cc2a1cf545571e3e5a131992f6b4561c60614be
Richard Lou <53197263+ruizhelou@users.noreply.github.com>
2026-01-12 22:23:26 +1300

    Complete factorial.js

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,17 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if(typeof(n) !== 'number') {
+        return undefined
+    }
+    if(n < 0) {
+        return undefined
+    }
+    if(!Number.isInteger(n)) {
+        return undefined
+    }
+    if(n <= 0) {
+        return 1
+    }
+    return n * factorial(n - 1)
 };
 
 // Do not edit below this line
